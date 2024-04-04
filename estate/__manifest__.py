85ae6bcdecd38ee6b9d034f113d93ae7b505387b
“Heriberto” <sosaheriberto2001@gmail.com>
2024-04-04 11:16:52 -0400

    Create basi data for odoo module, create model and properties

---
+++ estate/__manifest__.py
@@ -0,0 +1,20 @@
+
+{
+    'name': "HS Real State",
+    'version': "1.0",
+    'category': 'DEV',
+    'summary': """
+        No hello world
+    """,
+    'description': """
+        My first Odoo App, no hello world
+    """,
+    'author': "Heriberto Sosa",
+    'email': "sosaheriberto2001@gmail.com",
+    'depends': [
+        'base',
+    ],    
+    'application': True,
+
+
+}
