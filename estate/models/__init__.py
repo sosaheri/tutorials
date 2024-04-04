5e64b27fbedb0efa233962cbb561fa12ab11fa7c
“Heriberto” <sosaheriberto2001@gmail.com>
2024-04-04 11:17:59 -0400

    add models and properties to module estate

--- estate/models/__init__.py
+++ estate/models/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import models
-
-#from odoo import api, SUPERUSER_ID
+from . import estate_property
