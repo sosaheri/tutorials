baa4e1e7cb0805c49868fa3ca7250a81baeed625
“Heriberto” <sosaheriberto2001@gmail.com>
2024-04-05 09:09:30 -0400

    Add actions and menus to show module

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -16,7 +16,9 @@
     ],    
     'application': True,
     "data": [
-        "models/data/security/ir.model.access.csv"
+        "models/data/security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml"
     ],
 
 
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,25 +1,33 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models
-
+from dateutil.relativedelta import relativedelta
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Properties"
-    _order = "sequence"
 
-    name = fields.Char('Property Name', required=True, translate=True)
-    description = fields.Text('Property Description', translate=True)
-    postcode = fields.Char('Property postcode', translate=True)
-    date_availability = fields.Date('Property availability', translate=True)
-    expected_price = fields.Float('Property expected price', required=True, translate=True)
-    selling_price = fields.Float('Property selling price', translate=True)
-    bedrooms = fields.Integer('Property number bedrooms', translate=True)
-    living_area = fields.Integer('Property living', translate=True)
-    facades = fields.Integer('Property facades', translate=True)
-    garage = fields.Boolean('Property garage', translate=True)
-    garden = fields.Boolean('Property garde', translate=True)
-    garden_area = fields.Integer('Property area garde', translate=True)
+    active = fields.Boolean('Active', default=False)
+
+    name = fields.Char('Property Name', required=True)
+    description = fields.Text('Property Description')
+    postcode = fields.Char('Property postcode')
+    date_availability = fields.Date('Property availability', copy=False, default=lambda self: fields.Date.today() + relativedelta(months=3))
+    expected_price = fields.Float('Property expected price', required=True)
+    selling_price = fields.Float('Property selling price', readonly=True, copy=False)
+    bedrooms = fields.Integer('Property number bedrooms', default=2)
+    living_area = fields.Integer('Property living')
+    facades = fields.Integer('Property facades')
+    garage = fields.Boolean('Property garage')
+    garden = fields.Boolean('Property garde')
+    garden_area = fields.Integer('Property area garde')
     garden_orientation  = fields.Selection(
         string='Orientation',
         selection=[('North', 'North'), ('South', 'South'), ('West', 'West'), ('East', 'East')],
         help="DEfine orientation")
+    state = fields.Selection(
+            [('new', 'New'),
+             ('offer_received', 'Offer Received'),
+             ('offer_accepted', 'Offer Accepted'),
+             ('sold', 'Sold '),
+             ('canceled', 'Canceled ')], 
+        'State', default='new', copy=False, required=True)
